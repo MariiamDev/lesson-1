00ebfa98307dbeda50b6464bf3892ca40ed96b6b
MariiamDev <m.musabalieva@gmail.com>
2023-02-21 18:26:11 +0600

    main

---
+++ script.js
@@ -0,0 +1,61 @@
+//function
+
+//1- declaration
+//2- expression
+//3-arrow function -стрелочное функ.я
+
+//----------------------------
+
+//1-deslaration
+
+//  function myFunction(){
+// 	console.log('My function 1');
+//  }
+
+
+// alert()
+
+
+
+//-----------------------------------
+//2-expression
+// let myFunction2 = function(){
+// 	console.log('my function 2');
+// }
+// 	myFunction2()
+
+//--------------------------------------
+//3-arrow funcion
+// let myFunction3 = () => {
+// 	console.log('my function 3')
+// }
+
+// myFunction3();
+
+//-----------------------------------myFunction()
+//arguments
+
+// function helloUser(arg) {
+// 	console.log(`hello ${arg}`);
+// }
+// helloUser('Urmat')
+// helloUser('Shumkar')
+
+// let num1 = parseInt(prompt ('Введите первое число...'))
+// let op = prompt('Введите +,-')
+// let num2 = parseInt(prompt ('Введите второе число...'))
+// function calculator(a,b,c) {
+// 	if (b == '+') {
+// 		alert(a + b)
+// 	}else if ( b == '-') {
+// 		alert( a- c)
+// 	}else {
+// 		alert(false)
+// 	}
+// }
+
+// calculator(num1,op,num2)
+
+function sayHi(arg){
+	alert(arg)
+}
